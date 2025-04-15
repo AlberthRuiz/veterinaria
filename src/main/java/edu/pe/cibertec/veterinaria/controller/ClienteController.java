2e45c83b0b6808352b6a4d8fbb6a385490e66db5
SUITE <DW1@mail.com>
2025-04-14 21:09:03 -0500

    Vista cliente

--- src/main/java/edu/pe/cibertec/veterinaria/controller/ClienteController.java
+++ src/main/java/edu/pe/cibertec/veterinaria/controller/ClienteController.java
@@ -5,10 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 public class ClienteController {
+    @GetMapping()
+    public String listadoClientes(){
+        return "clientes-list";
+    }
+
+
+
     @GetMapping("/")
     public String mostrarPaginaInicio() {
         return "index";
     }
 }
+
