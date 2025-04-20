c9f09263c8f30c9b96a76166ca3753e0e17d7eea
SUITE <DW1@mail.com>
2025-04-20 12:18:22 -0500

    Reorder

--- src/main/java/edu/pe/cibertec/veterinaria/controller/ClienteController.java
+++ src/main/java/edu/pe/cibertec/veterinaria/controller/ClienteController.java
@@ -1,15 +1,26 @@
 package edu.pe.cibertec.veterinaria.controller;
 
+import edu.pe.cibertec.veterinaria.model.Cliente;
+import edu.pe.cibertec.veterinaria.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
-    @GetMapping()
-    public String listadoClientes(){
-        return "clientes-list";
+    @Autowired
+    ClienteService clienteService;
+
+    @GetMapping
+    public String listarClientes(Model model) {
+        List<Cliente> clientes = clienteService.listar();
+        model.addAttribute("clientes", clientes);
+        return "clientes/clientes-list";
     }
 
 
