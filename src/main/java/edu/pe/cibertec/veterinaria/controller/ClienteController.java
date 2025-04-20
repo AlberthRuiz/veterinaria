c860607d68e726045c1a2baef04c78ab87ed6fa2
SUITE <DW1@mail.com>
2025-04-20 14:03:49 -0500

    Ver cliente - OK

--- src/main/java/edu/pe/cibertec/veterinaria/controller/ClienteController.java
+++ src/main/java/edu/pe/cibertec/veterinaria/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,6 +38,15 @@ public class ClienteController {
     @GetMapping("/")
     public String mostrarPaginaInicio() {
         return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String verCliente(@PathVariable("id") int id, Model model){
+        Cliente cliente = clienteService.obtener(id);
+        if(cliente == null )
+            return "redirect:/clientes";
+        model.addAttribute("cliente", cliente);
+        return   "clientes/cliente-detail";
     }
 }
 
